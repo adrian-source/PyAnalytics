8b1a2600f32fe695aaa9ac852d0e4ee61b231008
adrian-source <adrian.sitterle@gmail.com>
2014-11-07 22:09:10 -0700

    Create flask-run.py

---
+++ flask-run.py
@@ -0,0 +1,117 @@
+#!flask/bin/python
+from app import app
+from flask import Flask, request
+from flask.ext.restful import Resource, Api
+from flask.ext.sqlalchemy import SQLAlchemy
+import datetime, os.path
+from sqlalchemy.exc import IntegrityError
+
+
+app = Flask(__name__)
+api = Api(app)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+class Key(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	key = db.Column(db.String(20), unique=True)
+	email = db.Column(db.String(20), unique=False)
+	appname = db.Column(db.String(20), unique=False)
+	created = db.Column(db.DateTime)
+
+	def __init__(self, key, nickname, email):
+		self.key = key
+		self.nickname = nickname
+		self.email = email
+		self.created = datetime.datetime.utcnow()
+		
+
+	def __repr__(self):
+		return "<Key %r>" % self.email
+
+class Log(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	key_id = db.Column(db.Integer, db.ForeignKey('key.id'))
+	text = db.Column(db.String(100), unique=False)
+	log_type = db.Column(db.String(10), unique=False)
+
+	def __init__(self, log_type, key_id, text):
+		self.log_type = log_type 
+		self.key_id = key_id
+		self.text = text
+
+	def __repr__(self):
+		return "<Log %r>" % self.text
+
+'''
+# USE THIS TO CREATE DATABASE
+db.create_all()
+from config import SQLLCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+'''
+
+class PyAnalyticsAdd(Resource):
+    def get(self):
+	key = request.form["key"]
+	key_db = Key.query.filter_by(key=key).first()
+	if key_db != None:
+		count = Log.query.filter_by(key_id = key_db.id).count()
+		return {'count': count}
+	else:
+		return {'status': 400, 'message': 'invalid key'}
+
+
+    def put(self):
+	log_type = request.form["log_type"]
+	key = request.form["key"]
+	text = request.form["text"]
+        
+	# validate key
+	key_db = Key.query.filter_by(key=key).first()
+	if key_db != None:
+		try:
+			log = Log(log_type, key_db.id, text)
+			db.session.add(log)
+			db.session.commit()
+		except IntegrityError:
+			return {'status': 400, 'message': 'database returned an error'}
+	else:
+		return {'status': 400, 'message': 'invalid key'}
+		
+        return 200
+
+class PyAnalyticsRegister(Resource):
+	def get(self):
+		email = request.form["email"]
+		keys = Key.query.filter_by(email=email)
+		jsonresp = "{"
+		for key in keys:
+			jsonresp += "%s: %s, " % (key.email, key.key)
+		jsonresp = jsonresp[:-2] + "}"
+		return jsonresp
+	
+	def put(self):
+		appname = request.form["appname"]
+		email = request.form["email"]
+		datenow = datetime.datetime.now()
+		new_key = datenow.strftime("%y%m%d%H%M%S")+appname
+		try:
+			key = Key(new_key, appname, email)
+			db.session.add(key)
+			db.session.commit()
+		except IntegrityError:
+			return {'status': 400, 'message': 'database returned an error'}
+		
+		return "{key:%s}" % new_key
+
+api.add_resource(PyAnalyticsAdd, '/log')
+api.add_resource(PyAnalyticsRegister, '/register')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
