3d28015a13827d411f6dc729dc7192999dbf9583
adrian-source <adrian.sitterle@gmail.com>
2014-11-09 23:30:57 -0700

    Update PyAnalytics_test.py

--- PyAnalytics_test.py
+++ PyAnalytics_test.py
@@ -1,13 +1,23 @@
 
 import PyAnalytics
-import random, string
+import random, string, os
 from time import sleep
+import json
 
-key = "" # PUT YOUR KEY HERE
+key = "141109230052app4" # PUT YOUR KEY HERE
 pyanal = PyAnalytics.PyAnalytics(key)
+
+result = json.loads(os.popen('curl http://localhost:5000/log -d "key='+key+'" -X GET').read())
+count = result["count"]
 
 for i in range(0, 1000):
 	cmd = ''.join(random.choice(string.ascii_uppercase) for i in range(40))
 	pyanal.log_use(cmd)
 	sleep(0.01)
-
+	result = json.loads(os.popen('curl http://localhost:5000/log -s -d "key='+key+'" -X GET').read())
+	if result["count"] == count+1:
+		count += 1
+		print count
+	else:
+		print "FAIL! "+str(count)
+		break
